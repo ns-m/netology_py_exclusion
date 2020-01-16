816c8e5a67f845385ed216b9b7709df189fdca2a
kmv <9629095@gmail.com>
2020-01-16 12:21:52 +0300

    First step (created files, start writing classes)

---
+++ exceptions_class.py
@@ -0,0 +1,8 @@
+class Exceptions:
+
+    def __init__(self, operators=input("Введите оператор : "), operand1=int(input("Введите операнд №1 : ")),
+                 operand2=int(input("Введите операнд №2 : "))):
+        self.operators = operators
+        self.operand1 = operand1
+        self.operand2 = operand2
+
---
+++ polish_notation.py
@@ -0,0 +1,6 @@
+from exceptions_class import Exceptions
+
+while True:
+    Exceptions()
+    break
+
