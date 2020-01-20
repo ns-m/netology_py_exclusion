cc48feaed5ca70507deb43a524bdf269dd899802
kmv <9629095@gmail.com>
2020-01-20 09:16:12 +0300

    Polish notation final

--- exceptions_class.py
+++ exceptions_class.py
@@ -11,27 +11,31 @@ class Exceptions:
         assert (self.operators in operators_list), 'Введите один из данных операторов: +, -, *, /'
 
     def exceptions_operands(self):
+        if self.operand1 == '0' or self.operand2 == '0':
+            self.operand1, self.operand2 = int(0), int(0)
+        if self.operand1 == '' or self.operand2 == '':
+            try:
+                self.operand1, self.operand2 = int(self.operand1), int(self.operand2)
+            except ValueError:
+                print('введены не все операнды')
         try:
-            int(self.operand1) / int(self.operand2)
+            self.operand1 / self.operand2
         except ArithmeticError:
             print('арифметическая ошибка')
         except TypeError:
             print('вводить только числа')
-        except ValueError:
-            print('введены не все операнды')
 
     def operation(self):
         result = 0
-        operand1, operand2 = int(self.operand1), int(self.operand2)
         try:
             if self.operators == '/':
-                result = operand1 / operand2
+                result = int(self.operand1) / int(self.operand2)
             elif self.operators == '+':
-                result = operand1 + operand2
+                result = int(self.operand1) + int(self.operand2)
             elif self.operators == '*':
-                result = operand1 * operand2
+                result = int(self.operand1) * int(self.operand2)
             elif self.operators == '-':
-                result = operand1 - operand2
+                result = int(self.operand1) - int(self.operand2)
         except:
             result = ''
         return result
--- polish_notation.py
+++ polish_notation.py
@@ -6,7 +6,7 @@ while True:
     #Exceptions().exceptions_operands()
 
     if Exceptions().operation():
-        print(f'Выражение : {Exceptions().operators} {Exceptions().operand1} {Exceptions().operand2} = {Exceptions().operation()}')
+        print(f'Выражение:  {Exceptions().operators} {Exceptions().operand1} {Exceptions().operand2} = {Exceptions().operation()}')
     else:
         print(Exceptions().exceptions_operands())
     break
