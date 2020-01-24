f4b6d198f5b8afeae40f215229c809d05d8a9232
kmv <9629095@gmail.com>
2020-01-24 16:02:07 +0300

    Fix 2 beta

--- exceptions_class.py
+++ exceptions_class.py
@@ -1,24 +1,27 @@
 class PolishCalculator:
 
-    def __init__(self, operators, operand1, operand2):
-        self.operators = operators
-        self.operand1 = operand1
-        self.operand2 = operand2
+    def __init__(self, polish_input):
+        self.polish_input = polish_input
+        if self.polish_input[1] == ' ':
+            split_data = self.polish_input.split()
+        else:
+            split_data = list(self.polish_input)
+        self.split_data = split_data
 
     def validate_operators(self):
         operators_list = ['+', '-', '*', '/']
-        assert (self.operators in operators_list), 'Введите один из данных операторов: +, -, *, /'
+        assert (self.split_data[0] in operators_list), 'Введите один из данных операторов: +, -, *, /'
 
     def validate_operands(self):
-        if self.operand1 == '0' or self.operand2 == '0':
-            self.operand1, self.operand2 = int(0), int(0)
-        if self.operand1 == '' or self.operand2 == '':
+        if self.split_data[1] == '0' or self.split_data[2] == '0':
+            self.split_data[1], self.split_data[2] = int(0), int(0)
+        if self.split_data[1] == '' or self.split_data[2] == '':
             try:
-                self.operand1, self.operand2 = int(self.operand1), int(self.operand2)
+                self.split_data[1], self.split_data[2] = int(self.split_data[1]), int(self.split_data[2])
             except ValueError:
                 print('введены не все операнды')
         try:
-            self.operand1 / self.operand2
+            self.split_data[1] / self.split_data[2]
         except ArithmeticError:
             print('арифметическая ошибка')
         except TypeError:
@@ -28,14 +31,14 @@ class PolishCalculator:
         result = 0
         self.result = result
         try:
-            if self.operators == '/':
-                self.result = int(self.operand1) / int(self.operand2)
-            elif self.operators == '+':
-                self.result = int(self.operand1) + int(self.operand2)
-            elif self.operators == '*':
-                self.result = int(self.operand1) * int(self.operand2)
-            elif self.operators == '-':
-                self.result = int(self.operand1) - int(self.operand2)
+            if self.split_data[0] == '/':
+                self.result = int(self.split_data[1]) / int(self.split_data[2])
+            elif self.split_data[0] == '+':
+                self.result = int(self.split_data[1]) + int(self.split_data[2])
+            elif self.split_data[0] == '*':
+                self.result = int(self.split_data[1]) * int(self.split_data[2])
+            elif self.split_data[0] == '-':
+                self.result = int(self.split_data[1]) - int(self.split_data[2])
         except:
             self.result = ''
         return self.result
--- polish_notation.py
+++ polish_notation.py
@@ -1,13 +1,15 @@
 from exceptions_class import PolishCalculator
 
-calculations  = PolishCalculator(input("Введите оператор : "), input("Введите операнд №1 : "), input("Введите операнд №2 : "))
+polish_input = input("Введите данные: ")
+
+calculations  = PolishCalculator(polish_input)
 
 while True:
 
     calculations.validate_operators()
 
     if calculations.operation():
-        print(f'Выражение:  {calculations.operators} {calculations.operand1} {calculations.operand2} = {calculations.operation()}')
+        print(f'Выражение:  {calculations.split_data[0]} {calculations.split_data[1]} {calculations.split_data[2]} = {calculations.operation()}')
     else:
         print(calculations.validate_operands())
     break
